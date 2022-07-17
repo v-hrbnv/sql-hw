c9965181e4e7005f329a8e76f5bc688cfbac2665
Vadym Horbanov <vadym.horbanov.contractor@bydeluxe.com>
2022-07-17 13:50:35 +0300

    queries pt2

--- queries.sql
+++ queries.sql
@@ -1,21 +1,30 @@
+--get all usernames
 SELECT Username FROM Users;
 
+--count users that have username like vasya
 SELECT COUNT(Username)
 FROM Users
 WHERE (lower(Username) LIKE '%vasya%');
 
+--get 10 first posts
 SELECT TOP 10 Title, Text, Created_date
 FROM Posts;
 
+--get 10 first posts + attachments
 SELECT TOP 10 Posts.Title, Posts.Text, Posts.Created_date, Attachments.Img_url
-FROM Posts p
+FROM Posts
 LEFT JOIN Attachments ON Posts.ID = Attachments.Post_id;
 
+--get 10 first posts + attachments + usenames
 SELECT TOP 10 Posts.Title, Posts.Text, Posts.Created_date, Attachments.Img_url, Users.Username
-FROM Posts p
+FROM Posts
 LEFT JOIN Attachments ON Posts.ID = Attachments.Post_id
 LEFT JOIN Users ON Posts.Author_id = Users.ID;
 
+--get avg number of user comments
 SELECT AVG(a.rcount) FROM
 (SELECT COUNT(*) as rcount FROM Comments c GROUP BY c.Post_id) a;
 
+--get avg number of comments per user
+SELECT AVG(a.rcount) FROM
+(SELECT COUNT(*) as rcount FROM Comments c GROUP BY c.Author_id) a;
